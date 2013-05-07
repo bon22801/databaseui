efa5a92f8342576dce65930d4a2db71e18fcc63a
Yu Jer Lin <bon22801@gmail.com>
2013-05-07 15:36:24 -0400

    adding export

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,12 +1,18 @@
 class Node < ActiveRecord::Base
 
 	 # Show action for autocomplete
+
+
+
+  
+
+
   def display_node
 	"#{self.working_name}"  
   end
 
 
- STATUS = %w[Native NOT_NATIVE UNDEFINED]
+  STATUS = %w[Native NOT_NATIVE UNDEFINED]
   attr_accessible :functional_group_id, :is_assemblage, :itis_id, :non_itis_id, :user_id, :working_name, :project_id, :mod, :approved, :native_status
   belongs_to :functional_group
   belongs_to :user
@@ -21,4 +27,19 @@ class Node < ActiveRecord::Base
   validates :itis_id, :presence => true
   validates :functional_group_id, :presence => true
   validates :is_assemblage, :presence => true
+
+
+   
+    def self.to_csv (options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names.first(8)
+      all.each do |node| 
+       csv << node.attributes.values_at(*column_names.first(8))
+
+        end
+      end
+    end
+
+    
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 201303260500056) do
+ActiveRecord::Schema.define(:version => 201309260500054) do
 
   create_table "author_cites", :force => true do |t|
     t.integer  "author_id",                      :null => false
@@ -80,6 +80,11 @@ ActiveRecord::Schema.define(:version => 201303260500056) do
     t.boolean  "approved",   :default => false
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+  end
+
+  create_table "exports", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "facilitation_interaction_observations", :force => true do |t|
